ac552e74aaa1dca2e1c5ade4f78ede1b83b5dd8e
AmozasXleb <amozonka56@gmail.com>
2025-06-05 00:59:32 +0500

    добавление функции очистки кнопкой

--- src/pages/LogInPage/LogIn.tsx
+++ src/pages/LogInPage/LogIn.tsx
@@ -3,18 +3,25 @@ import "./css.css";
 
 export function LogIn() {
   const telDiv = useRef<HTMLDivElement>(null);
+  const telInput = useRef<HTMLInputElement>(null);
 
-  function onFocus() {
+  const onFocus = () => {
     if (telDiv.current) {
       telDiv.current.style.border = "1px solid #EDCAB8";
     }
-  }
+  };
 
-    function onBlur() {
+  const onBlur = () => {
     if (telDiv.current) {
       telDiv.current.style.border = "1px solid #F2F2F5";
     }
-  }
+  };
+
+  const clearInput = () => {
+    if (telInput.current) {
+      telInput.current.value = "";
+    }
+  };
 
   return (
     <main>
@@ -28,13 +35,13 @@ export function LogIn() {
             </p>
           </div>
 
-          <div className="input-cont" >
+          <div className="input-cont">
             <label>Номер Телефона</label>
             <div ref={telDiv}>
               <img src="../src/assets/ru_flag.svg" alt="Ru" />
               <span>+7</span>
-              <input type="tel" onFocus={onFocus} onBlur={onBlur} />
-              <button>
+              <input type="tel" ref={telInput} onFocus={onFocus} onBlur={onBlur} />
+              <button onClick={clearInput}>
                 <img src="../src/assets/delete_icon.svg" alt="Очистить" />
               </button>
             </div>
